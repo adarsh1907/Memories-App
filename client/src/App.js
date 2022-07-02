59f08ba2bb06320bf00ad573de8c827217eaeedf
Adarsh Gupta <69007272+adarsh1907@users.noreply.github.com>
2022-07-02 12:39:44 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { Container} from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Auth from './components/Auth/Auth';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
+// GET CURRENT ID
+const App = () =>(
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+    
+  );
+
+export default App;
